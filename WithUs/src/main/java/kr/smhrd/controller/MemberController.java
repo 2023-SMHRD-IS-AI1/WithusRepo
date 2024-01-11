d39131ca8da4648d54760649bc541cc91adb17e7
smhrd <smhrd@211.228.36.152>
2024-01-11 16:46:53 +0900

    d

--- WithUs/src/main/java/kr/smhrd/controller/MemberController.java
+++ WithUs/src/main/java/kr/smhrd/controller/MemberController.java
@@ -1,6 +1,6 @@
 package kr.smhrd.controller;
 
-import java.util.List;
+
 
 import javax.servlet.http.HttpSession;
 
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.mysql.jdbc.interceptors.SessionAssociationInterceptor;
+
 
 import kr.smhrd.entity.Member;
 import kr.smhrd.entity.Message;
