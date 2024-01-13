7eb6fdcbca9fb6b4d9527022d6c369333ce21453
lee <wlsrms4559@gmail.com>
2024-01-13 16:56:14 +0900

    일단 커밋

--- WithUs/src/main/java/kr/smhrd/controller/MemberController.java
+++ WithUs/src/main/java/kr/smhrd/controller/MemberController.java
@@ -32,168 +32,168 @@ import kr.smhrd.mapper.MessageMapper;
 @Controller
 public class MemberController {
 
-	// class 안에 있는 메소드를 쓰려면 '객체생성' 필요함, interface는 객체생성도 안됨 -> but 스프링에서는 생성하는게 아니라 주입받아서 씀
-	@Autowired // 스프링 컨테이너에 객체가 생성되어 올라간 boardMapper 객체를 주입받아 사용하겠다
-	private MemberMapper memberMapper; // DAO같은 역할인데 DAO는 커넥션 관리까지 다했다면
-	
-	
-	
-	// @RequestMapping : get방식,post방식 요청을 다 받을 수 있음
-	// @GetMapping : get방식 요청만 받을 수 있음
-	// @PostMapping : post방식 요청만 받을 수 있음
-	
-	
-	@RequestMapping("/")
-	public String main() {
-		return "Main";
-	}
-	
-	@RequestMapping("/goMain")
-	public String goMain() {
-		return "Main";
-	}
-	
-	@RequestMapping("/goLogin")
-	public String goLogin() {
-		return "Login";
-	}
-	
-	@RequestMapping("goLogout")
-	public String goLogout(HttpSession session) {
-		session.removeAttribute("loginMember");
+   // class 안에 있는 메소드를 쓰려면 '객체생성' 필요함, interface는 객체생성도 안됨 -> but 스프링에서는 생성하는게 아니라 주입받아서 씀
+   @Autowired // 스프링 컨테이너에 객체가 생성되어 올라간 boardMapper 객체를 주입받아 사용하겠다
+   private MemberMapper memberMapper; // DAO같은 역할인데 DAO는 커넥션 관리까지 다했다면
+   
+   
+   
+   // @RequestMapping : get방식,post방식 요청을 다 받을 수 있음
+   // @GetMapping : get방식 요청만 받을 수 있음
+   // @PostMapping : post방식 요청만 받을 수 있음
+   
+   
+   @RequestMapping("/")
+   public String main() {
+      return "Main";
+   }
+   
+   @RequestMapping("/goMain")
+   public String goMain() {
+      return "Main";
+   }
+   
+   @RequestMapping("/goLogin")
+   public String goLogin() {
+      return "Login";
+   }
+   
+   @RequestMapping("goLogout")
+   public String goLogout(HttpSession session) {
+      session.removeAttribute("loginMember");
 
-		return "Main";
-	}
+      return "Main";
+   }
 
-	@RequestMapping("/goChat")
-	public String goChat() {
-		return "Chat";
-	}
-	@RequestMapping("/goJoin")
-	public String goJoin() {
-		return "Join";
-	}
+   @RequestMapping("/goChat")
+   public String goChat() {
+      return "Chat";
+   }
+   @RequestMapping("/goJoin")
+   public String goJoin() {
+      return "Join";
+   }
 
-	@RequestMapping("/goProfil")
-	public String goProfil() {
-		return "Profil";
-	}
-	
-	@RequestMapping("/goFollow")
-	public String goFollow() {
-		return "Follow";
-	}
-	
-	@RequestMapping("/goUserproinfo")
-	public String goUserproinfo() {
-		return "Userproinfo";
-	}
-	
-	@RequestMapping("/goUserinfo")
-	public String goUserinfo() {
-		return "Userinfo";
-	}
-	
-	@RequestMapping("/goDaily")
-	public String goDaily() {
-		return "daily";
-	}
-	
-//	@RequestMapping("/goResult")
-//	public String goResult(Member member, Model model) {
-//		
-//		memberMapper.memberInsert(member);
-//		model.addAttribute("mb_id",member.getMb_id());
-//		model.addAttribute("mb_nick", member.getMb_nick());
-//		
-//	
-//		
-//		return "result";
-//	}
-	
-	@RequestMapping("/goResult")
-	public String goResult(Member member, Model model) {
-	    memberMapper.memberInsert(member);
-	    model.addAttribute("mb_id", member.getMb_id());
+   @RequestMapping("/goProfil")
+   public String goProfil() {
+      return "Profil";
+   }
+   
+   @RequestMapping("/goFollow")
+   public String goFollow() {
+      return "Follow";
+   }
+   
+   @RequestMapping("/goUserproinfo")
+   public String goUserproinfo() {
+      return "Userproinfo";
+   }
+   
+   @RequestMapping("/goUserinfo")
+   public String goUserinfo() {
+      return "Userinfo";
+   }
+   
+   @RequestMapping("/goDaily")
+   public String goDaily() {
+      return "daily";
+   }
+   
+//   @RequestMapping("/goResult")
+//   public String goResult(Member member, Model model) {
+//      
+//      memberMapper.memberInsert(member);
+//      model.addAttribute("mb_id",member.getMb_id());
+//      model.addAttribute("mb_nick", member.getMb_nick());
+//      
+//   
+//      
+//      return "result";
+//   }
+   
+   @RequestMapping("/goResult")
+   public String goResult(Member member, Model model) {
+       memberMapper.memberInsert(member);
+       model.addAttribute("mb_id", member.getMb_id());
 
 
-	    return "result";
-	}
-	
-	
-	
+       return "result";
+   }
+   
+   
+   
 
-	@RequestMapping("/surveyInsert")
-	public String surveyInsert(Survey survey) {
-		System.out.println(survey.toString());
-		
-		
-		
-		return "Main";
-	}
-	
-	// 로그인
-	@RequestMapping("/memberSelect")
-	public String memberSelect(Member member, HttpSession session) {
-		Member loginMember = memberMapper.memberSelect(member);
-		session.setAttribute("loginMember", loginMember);
-		
-		LocalDate currentDate = LocalDate.now();
-		if(loginMember != null) {
-			
+   @RequestMapping("/surveyInsert")
+   public String surveyInsert(Survey survey) {
+      System.out.println(survey.toString());
+      
+      
+      
+      return "Main";
+   }
+   
+   // 로그인
+   @RequestMapping("/memberSelect")
+   public String memberSelect(Member member, HttpSession session) {
+      Member loginMember = memberMapper.memberSelect(member);
+      session.setAttribute("loginMember", loginMember);
+      
+      LocalDate currentDate = LocalDate.now();
+      if(loginMember != null) {
+         
 
-	    // Member 객체에서 생년월일 String을 LocalDate로 변환
-	    LocalDate birthDate = LocalDate.parse(loginMember.getMb_birthdate(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+       // Member 객체에서 생년월일 String을 LocalDate로 변환
+       LocalDate birthDate = LocalDate.parse(loginMember.getMb_birthdate(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-	    int mb_age = Period.between(birthDate, currentDate).getYears(); // 나이 계산
-	    System.out.println(mb_age);
-	    session.setAttribute("mb_age", mb_age);
-	    return "Main";
-		
-		}else {
-			return "Login";
-			
-		}
-		
-		
-	}
-	
+       int mb_age = Period.between(birthDate, currentDate).getYears(); // 나이 계산
+       System.out.println(mb_age);
+       session.setAttribute("mb_age", mb_age);
+       return "Main";
+      
+      }else {
+         return "Login";
+         
+      }
+      
+      
+   }
+   
 
-	
-	
-	
-		
-	// 서울 지도 이동
-	@RequestMapping("/goSeoul")
-	public String goSeoul() {
-		return "Seoul";
-	}
-	// 강릉 지도 이동
-	@RequestMapping("/goGr")
-	public String goGr() {
-		return "Grmap";
-	}
-	// 제주 지도 이동
-	@RequestMapping("goJj")
-	public String goJj() {
-		return "jjmap";
-	}
-	// 부산 지도 이동
-	@RequestMapping("goBusan")
-	public String goBusan() {
-		return "busan";
-	}
-	
-	
-	@RequestMapping("/goGrwriter")
-	public String goGrwriter() {
-		return "gr_writer";
-	}
-	
-	@RequestMapping("/goRewrite")
-	public String goRewrite() {
-		
-		return "re_writer";
-	}
-	
+   
+   
+   
+      
+   // 서울 지도 이동
+   @RequestMapping("/goSeoul")
+   public String goSeoul() {
+      return "Seoul";
+   }
+   // 강릉 지도 이동
+   @RequestMapping("/goGr")
+   public String goGr() {
+      return "Grmap";
+   }
+   // 제주 지도 이동
+   @RequestMapping("goJj")
+   public String goJj() {
+      return "jjmap";
+   }
+   // 부산 지도 이동
+   @RequestMapping("goBusan")
+   public String goBusan() {
+      return "busan";
+   }
+   
+   
+   @RequestMapping("/goGrwriter")
+   public String goGrwriter() {
+      return "gr_writer";
+   }
+   
+   @RequestMapping("/goRewrite")
+   public String goRewrite() {
+      
+      return "re_writer";
+   }
+   
 }
--- WithUs/src/main/webapp/WEB-INF/views/Main.jsp
+++ WithUs/src/main/webapp/WEB-INF/views/Main.jsp
@@ -4,7 +4,7 @@
 <%@page import="kr.smhrd.entity.Member"%>
 <%@page import="java.util.ArrayList"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+   pageEncoding="UTF-8"%>
 
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -31,7 +31,7 @@
 <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 </head>
 <body>
-	<%@ include file="./nav.jsp" %>
+   <%@ include file="./nav.jsp" %>
     <div id="main">
       <div id="main1"></div>
       <div id="main2"></div>
@@ -163,4 +163,3 @@
     <footer></footer>
 </body>
 </html>
-
